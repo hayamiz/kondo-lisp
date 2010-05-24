d18d9a3ef9f60f2081400e4083ae1042a24e3a55
Yuto Hayamizu <y.hayamizu@gmail.com>
2010-05-25 07:00:21 +0900

    add project file and .gitignore

---
+++ project.clj
@@ -0,0 +1,8 @@
+;; -*- indent-tabs-mode: nil; mode: clojure  -*-
+
+(defproject kondo-lisp "0.0.1-SNAPSHOT"
+  :description "A framework for dynamic prototyping on Arduino"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]
+                 [clj-match "0.0.1-SNAPSHOT"]]
+  :dev-dependencies [[leiningen/lein-swank "1.1.0"]])
