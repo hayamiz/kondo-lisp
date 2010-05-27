33e8d2e96785d2725b367bf7b66996f2c4aab199
Yuto Hayamizu <y.hayamizu@gmail.com>
2010-05-28 04:15:41 +0900

    implemented basic REPL interface for serial communication with Arduino

--- Makefile
+++ Makefile
@@ -4,25 +4,27 @@ STANDALONE = kondolisp-standalone.jar
 VMINST_H = sketch/kondo_lisp/vminst.h
 BUILTIN_H = sketch/kondo_lisp/builtin.h
 
-.PHONY: all clean check jar
+.PHONY: all clean check jar deps
 
 all: jar $(VMINST_H) $(BUILTIN_H)
 
+deps:
+	ln -sf /usr/share/java/RXTXcomm.jar ./lib/
+	lein deps
+
 jar: $(TARGET) $(STANDALONE)
 
-$(STANDALONE): $(shell find . -type f -name "*.clj")
-	lein deps
+$(STANDALONE): deps $(shell find . -type f -name "*.clj")
 	lein uberjar
 
-$(TARGET): $(shell find . -type f -name "*.clj")
-	lein deps
+$(TARGET): deps $(shell find . -type f -name "*.clj")
 	lein jar
 
 $(VMINST_H): $(STANDALONE)
-	java -client -jar $(STANDALONE) header vminst > $(VMINST_H)
+	java -client -jar $(STANDALONE) header vminst | egrep "^(\/\*|#define)" > $(VMINST_H)
 
 $(BUILTIN_H): $(STANDALONE)
-	java -client -jar $(STANDALONE) header builtin > $(BUILTIN_H)
+	java -client -jar $(STANDALONE) header builtin | egrep "^(\/\*|#define)" > $(BUILTIN_H)
 
 pom.xml: project.clj $(TARGET)
 	lein pom
--- src/kondolisp/main.clj
+++ src/kondolisp/main.clj
@@ -3,7 +3,27 @@
   (:use [clojure core]
 	[clojure.contrib pprint command-line]
 	[clj-match])
-  (:require [kondolisp compiler]))
+  (:require [kondolisp compiler serial])
+  (:import [sun.misc Signal SignalHandler]))
+
+(defn kondo-repl []
+  (kondolisp.serial/open-serial)
+  (try
+   (while true
+	  (let [serial-output (kondolisp.serial/read-until)]
+	    (.print System/out serial-output)
+	    (.flush System/out)
+	    (let [user-input (do (.print System/out "kondo-lisp> ")
+				 (read)),
+		  byteseq (kondolisp.compiler/kondo-compile user-input)]
+	      (kondolisp.serial/write-serial
+	       (concat byteseq [0 0 0 0])))))
+   (catch Exception e
+     (.println System/out (str e))
+     (.println System/out
+	       (format "message: %s" (.getMessage e))))
+   (finally
+    (kondolisp.serial/close-serial))))
 
 (defn -main [& argv]
   (match argv
@@ -12,6 +32,9 @@
     ;;
     ("header" "builtin" & rest)
     (kondolisp.compiler/generate-builtin-header)
+    ;;
+    ("repl" & rest)
+    (kondo-repl)
     ;;
     _	(.println System/out (str argv)))
   true)
