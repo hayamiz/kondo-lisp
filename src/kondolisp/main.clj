0f814f1ba552e0ee90d73dc97c74fd7027a7a167
Yuto Hayamizu <y.hayamizu@gmail.com>
2010-05-29 03:07:12 +0900

    use thread for reading outputs of serial port

--- src/kondolisp/main.clj
+++ src/kondolisp/main.clj
@@ -4,26 +4,38 @@
 	[clojure.contrib pprint command-line]
 	[clj-match])
   (:require [kondolisp compiler serial])
-  (:import [sun.misc Signal SignalHandler]))
+  (:import [sun.misc Signal SignalHandler]
+	   [java.util.concurrent LinkedBlockingQueue TimeUnit CyclicBarrier]))
 
 (defn kondo-repl []
   (kondolisp.serial/open-serial)
-  (try
-   (while true
-	  (let [serial-output (kondolisp.serial/read-until)]
-	    (.print System/out serial-output)
+  (let [output-queue (LinkedBlockingQueue. ),
+	serial-output-thread
+	(Thread. (fn []
+		   (while true
+			  (let [serial-output (kondolisp.serial/read-until)]
+			    (.put output-queue serial-output)))))]
+    (.start serial-output-thread)
+    (try
+     (while true
+	    (.print System/out (.take output-queue))
+	    (while (> (.size output-queue) 0)
+		   (.print System/out (.take output-queue)))
 	    (.flush System/out)
 	    (let [user-input (do (.print System/out "kondo-lisp> ")
 				 (read)),
 		  byteseq (kondolisp.compiler/kondo-compile user-input)]
 	      (kondolisp.serial/write-serial
-	       (concat byteseq [0 0 0 0])))))
-   (catch Exception e
-     (.println System/out (str e))
-     (.println System/out
-	       (format "message: %s" (.getMessage e))))
-   (finally
-    (kondolisp.serial/close-serial))))
+	       (concat byteseq [0 0 0 0]))
+	      (Thread/sleep 500)))
+     (catch Exception e
+       (.println System/out (str e))
+       (.println System/out
+		 (format "message: %s" (.getMessage e))))
+     (finally
+      (kondolisp.serial/close-serial)
+      (.destroy serial-output-thread)
+      ))))
 
 (defn -main [& argv]
   (match argv
