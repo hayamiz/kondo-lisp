65880a2a1df95c365922ee0af205839b1854932f
Yuto Hayamizu <y.hayamizu@gmail.com>
2010-05-30 07:21:38 +0900

    add main class for testing kondoView

---
+++ kondo-gui/src/kondolisp/gui/kondoViewRunner.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package kondolisp.gui;
+
+/**
+ *
+ * @author haya
+ */
+public class kondoViewRunner {
+    public static void main(String[] argv){
+        kondoView view = new kondoView();
+        view.setVisible(true);
+    }
+}
