2e2a0ff0b36148958612b4e0529bfbeea4645974
Yuto Hayamizu <y.hayamizu@gmail.com>
2011-10-02 13:58:35 +0900

    run.sh setup env vars correctly

--- run.sh
+++ run.sh
@@ -6,4 +6,14 @@ for file in $(ls lib/*.jar); do
 	CLASSPATH=$CLASSPATH:$file
 done
 
+for file in $(ls rxtx/*.jar); do
+	CLASSPATH=$CLASSPATH:$file
+done
+
+for file in $(ls kondo-gui/dist/*.jar); do
+	CLASSPATH=$CLASSPATH:$file
+done
+
+LD_LIBRARY_PATH="$(find `pwd`/rxtx -type d|while read dir; do echo -n "$dir:"; done):$LD_LIBRARY_PATH"
+
 java -client -classpath $CLASSPATH kondolisp.main $*
